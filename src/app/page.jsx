b93d435f94b334bb4e770a59e2c5b34ed485c9a0
diyar <diyardireki111@gmail.com>
2026-07-09 14:03:53 +0300

    add Vanta library

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,4 +1,22 @@
+import ParticlesBackground from "@/components/Background";
 import "@/style/index.css";
+
 export default function Home() {
-  return <div></div>;
+  return (
+    <>
+      <ParticlesBackground />
+      <main
+        style={{
+          height: "100vh",
+          display: "grid",
+          placeItems: "center",
+          color: "#fff",
+          position: "relative",
+          zIndex: 1,
+        }}
+      >
+        <h1>dsadsa</h1>
+      </main>
+    </>
+  );
 }
--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -1,14 +1,16 @@
 import { useCallback, useEffect, useState } from "react";
 
 const useDarkMode = () => {
-  const [isDark, setIsDark] = useState(
-    JSON.parse(localStorage.getItem("isDark")) || false,
-  );
+  const [isDark, setIsDark] = useState(() => {
+    if (typeof window === "undefined") return false;
+    const saved = localStorage.getItem("isDark");
+    return saved ? JSON.parse(saved) : false;
+  });
 
   const changeMode = useCallback(() => {
     setIsDark((prev) => {
       const changedValue = !prev;
-      localStorage.setItem("isDark", changedValue);
+      localStorage?.setItem("isDark", changedValue);
       return changedValue;
     });
   }, []);
